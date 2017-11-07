e0b7f7d4fedbbc57cac517c901b4b37c42eb0259
Rafael Carlos <shanglangjunior@hotmail.com>
2017-11-07 10:31:17 -0300

    Configurações

---
+++ app/Produto.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace APIGas;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Produto extends Model
+{
+    //
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,14 +67,14 @@
     <body>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
+            <div class="top-right links">
+                @auth
+                <a href="{{ url('/home') }}">Home</a>
+                @else
+                <a href="{{ route('login') }}">Login</a>
+                <a href="{{ route('register') }}">Register</a>
+                @endauth
+            </div>
             @endif
 
             <div class="content">
@@ -87,7 +87,7 @@
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/RafaelCarlos">GitHub</a>
                 </div>
             </div>
         </div>
